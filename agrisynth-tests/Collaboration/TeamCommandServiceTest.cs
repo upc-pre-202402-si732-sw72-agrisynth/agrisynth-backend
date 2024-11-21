030a0a6a81ed5dd925f9e6a7e5559086f4a51bd8
Velarde Gonzales Nestor H <u20211c221@upc.edu.pe>
2024-11-21 17:15:20 -0500

    Aggregate Test Unit in bound context Machinery

---
+++ agrisynth-tests/Collaboration/TeamCommandServiceTest.cs
@@ -0,0 +1,46 @@
+﻿using agrisynth_backend.Collaboration.Application.CommandServices;
+using agrisynth_backend.Collaboration.Domain.Model.Commands;
+using agrisynth_backend.Collaboration.Domain.Model.Entities;
+using agrisynth_backend.Collaboration.Domain.Repositories;
+using agrisynth_backend.Shared.Domain.Repositories;
+using Moq;
+using Xunit;
+using Assert = NUnit.Framework.Assert;
+
+namespace agrisynth_tests.Collaboration;
+
+public class TeamCommandServiceTest
+{
+    private readonly Mock<ITeamRepository> _teamRepositoryMock;
+    private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+    private readonly TeamCommandService _teamCommandService;
+    
+    public TeamCommandServiceTest()
+    {
+        _teamRepositoryMock = new Mock<ITeamRepository>();
+        _unitOfWorkMock = new Mock<IUnitOfWork>();
+        _teamCommandService = new TeamCommandService(_teamRepositoryMock.Object, _unitOfWorkMock.Object);
+    }
+    
+    [Fact] 
+    // este test es para probar que se crea un equipo solo el nombre 
+    public async Task CreateTeamCommand_SuccessfullyCreatesTeam()
+    {
+        // Arrange
+        var command = new CreateTeamCommand(
+            "Team 1");
+        var team = new Team(command);
+
+        _teamRepositoryMock.Setup(repo => repo.AddAsync(It.IsAny<Team>())).Returns(Task.CompletedTask);
+        _unitOfWorkMock.Setup(uow => uow.CompleteAsync()).Returns(Task.CompletedTask);
+
+        // Act
+        var result = await _teamCommandService.Handle(command);
+
+       // Assert
+       Xunit.Assert.NotNull(result);
+       Xunit.Assert.Equal("Team 1", result.Name);
+        _teamRepositoryMock.Verify(repo => repo.AddAsync(It.IsAny<Team>()), Times.Once);
+        _unitOfWorkMock.Verify(uow => uow.CompleteAsync(), Times.Once);
+    }
+}
---
+++ agrisynth-tests/Machineryrental/MachineryCommandServiceTest.cs
@@ -0,0 +1,47 @@
+﻿using agrisynth_backend.Machineryrental.Application.CommandServices;
+using agrisynth_backend.Machineryrental.Domain.Model.Aggregates;
+using agrisynth_backend.Machineryrental.Domain.Model.Commands;
+using agrisynth_backend.Machineryrental.Domain.Repositories;
+using agrisynth_backend.Shared.Domain.Repositories;
+using Moq;
+using Xunit;
+
+namespace agrisynth_tests.Machineryrental;
+
+public class MachineryCommandServiceTest
+{
+    private readonly Mock<IMachineryRepository> _machineryRepositoryMock;
+    private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+    private readonly MachineryCommandService _machineryCommandService;
+    
+    public MachineryCommandServiceTest()
+    {
+        _machineryRepositoryMock = new Mock<IMachineryRepository>();
+        _unitOfWorkMock = new Mock<IUnitOfWork>();
+        _machineryCommandService = new MachineryCommandService(_machineryRepositoryMock.Object, _unitOfWorkMock.Object);
+    }
+    
+    [Fact]
+    // este test es para probar que se crea un equipo solo el nombre
+    public async Task CreateMachineryCommand_Successfull()
+    {
+        // Arrange
+        var command = new CreateMachineryCommand(
+            "Machinery 1", 
+            123,
+            "www.image.com");
+        var machinery = new Machinery(command);
+
+        _machineryRepositoryMock.Setup(repo => repo.AddAsync(It.IsAny<Machinery>())).Returns(Task.CompletedTask);
+        _unitOfWorkMock.Setup(uow => uow.CompleteAsync()).Returns(Task.CompletedTask);
+
+        // Act
+        var result = await _machineryCommandService.Handle(command);
+
+       // Assert
+       Xunit.Assert.NotNull(result);
+       Xunit.Assert.Equal("Machinery 1", result.Name);
+        _machineryRepositoryMock.Verify(repo => repo.AddAsync(It.IsAny<Machinery>()), Times.Once);
+        _unitOfWorkMock.Verify(uow => uow.CompleteAsync(), Times.Once);
+    }
+}
